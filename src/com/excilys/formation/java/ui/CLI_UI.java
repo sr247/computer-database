e53617afa9e288d87bf2392901f91526c7ef6d7b
Stéphane Romany <sromany@excilys.com>
2018-03-12 18:12:01 +0100

    Has add a feature command line ui

---
+++ src/com/excilys/formation/java/ui/CLI_UI.java
@@ -0,0 +1,14 @@
+package com.excilys.formation.java.ui;
+
+public class CLI_UI {
+	
+	
+	public void showDetailComputer() {
+		
+	}
+	
+	public static void main(String args) {
+		System.out.println("Test");
+	}
+
+}

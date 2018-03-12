55278080b702adde94042b6aa2db6a4b690ddc6c
Stéphane Romany <sromany@excilys.com>
2018-03-12 15:52:01 +0100

    Has add a Model Class Company for contained result of mapping

---
+++ src/com/excilys/formation/java/model/Company.java
@@ -0,0 +1,12 @@
+package com.excilys.formation.java.model;
+
+public class Company {
+	private int id;
+	private String name;
+	
+	
+	public Company(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+}

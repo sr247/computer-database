be967b70806499f191bcfa8a7d2632938b65843d
Stéphane Romany <sromany@excilys.com>
2018-03-14 15:07:51 +0100

    Has modified the way user interact with the system (generally the form of entering fields

--- src/com/excilys/formation/java/ui/CLI_UI.java
+++ src/com/excilys/formation/java/ui/CLI_UI.java
@@ -1,12 +1,12 @@
 package com.excilys.formation.java.ui;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.excilys.formation.java.exceptions.IncorrectFieldException;
-import com.excilys.formation.java.mapper.CompanyMP;
-import com.excilys.formation.java.mapper.ComputerMP;
+import com.excilys.formation.java.exceptions.InstanceNotFoundException;
+import com.excilys.formation.java.model.Company;
+import com.excilys.formation.java.model.Computer;
 import com.excilys.formation.java.service.WebServiceCompany;
 import com.excilys.formation.java.service.WebServiceComputer;
 
@@ -30,61 +30,120 @@ public class CLI_UI {
 		return exit;
 	}
 	
-	public void showDetailComputer(int id) {
-		System.out.println(wscmp.getComputerDB().getComputerByID(id));
+	public void showDetailComputer(String id) {
+		System.out.println(wscmp.getComputer(id));
 	}
 	
 	public void list(String[] t) {
 		boolean listAll =  t[1].equals("all");
 		String table = listAll ? t[2] : t[1];
 		if("computer".equals(table)) {
-			ArrayList<ComputerMP> cmpList = null;
+			ArrayList<Computer> cmpList = null;
 			if(listAll) {
-				// Programmation fonctionnel ? Peut etre. Envisager...
-				cmpList = wscmp.getComputerDB().getComputerList();
+				cmpList = wscmp.getList(0, 0);
 			} else {
-				// Gerer les cas from to
-				cmpList = wscmp.getComputerDB().getComputerList(0, 10);
+				cmpList = wscmp.getList(0, 10);
 			}
 
-			for(ComputerMP cmp : cmpList) {
+			for(Computer cmp : cmpList) {
 				System.out.println(cmp);
 			}
 		} else if("company".equals(table)){
-			ArrayList<CompanyMP> cpyList = null;
+			ArrayList<Company> cpyList = null;
 			
 			if(listAll) {
-				// Programmation fonctionnel ? Peut etre. Envisager...
-				cpyList = wscpy.getCompanyDB().getCompanyList();
+				// Modifier WebServiceCompany Comme Computer
+				cpyList = wscpy.getList(0, 0);
 			} else {
-				// Gerer les cas from to
-				cpyList = wscpy.getCompanyDB().getCompanyList(0, 10);
+				cpyList = wscpy.getList(0, 10);
 			}
-			for(CompanyMP cpn : cpyList) {
+			for(Company cpn : cpyList) {
 				System.out.println(cpn);
 			}
 		}
 	}
 	
 	public void create(String[] t) {
+		boolean err = false;
+		boolean exit = false;
 		boolean into = t[1].equals("into");
 		String table = into ? t[2] : t[1];
-		System.out.println("Enter the tuple corresponding:");
-		if("computer".equals(table)) {
-			// Ici il faudra check la forme et les valeurs de chaque ajout
-			// Valeur null, erreur de syntaxe, nombre de champ
-			String s = sc.nextLine();
-			String[] ss = s.replaceAll("[( | )]", " ").split(",");
-			try {
-				wscmp.create(ss);
-			}catch(IncorrectFieldException e) {
-				System.out.println("Error: invalid field provided");
+		
+		do{
+			if("computer".equals(table)) {
+				// Ici il faudra check la forme et les valeurs de chaque ajout
+				// Valeur null, erreur de syntaxe, nombre de champs
+				ArrayList<String> fields = new ArrayList<String>();
+				System.out.print("Entrez le nom: ");
+				fields.add(sc.nextLine());
+				System.out.print("Entrez la date d'achat: ");
+				fields.add(sc.nextLine());
+				System.out.print("Entrez la date de fin de production: ");
+				fields.add(sc.nextLine());
+	
+				// Ici une formulation différente 
+				System.out.print("Entrez l'id de l'entreprise propritaire: ");			
+				fields.add(sc.nextLine());
+				err = false;
+				try {
+					wscmp.createComputer(fields);
+				}catch(IncorrectFieldException e) {
+					System.err.println("Erreur: Champ invalide");
+					err = true;
+				}catch (InstanceNotFoundException e) {
+					System.err.println("Erreur: Instance inexistante");
+					err = true;
+				}
+			} else if("company".equals(table)) {
+				String s = sc.nextLine();
+				String[] ss = s.replaceAll("[( | )]", " ").split(",");			
+				wscpy.create(Integer.valueOf(ss[0]), ss[1]);
 			}
-		} else if("company".equals(table)) {
-			String s = sc.nextLine();
-			String[] ss = s.replaceAll("[( | )]", " ").split(",");			
-			wscpy.create(Integer.valueOf(ss[0]), ss[1]);
-		}
+		}while(err & !exit);
+	}
+	
+	public void delete(String id) {
+		wscmp.deleteComputer(id);
+	}
+	
+	public void update(String[] t) {
+		boolean err = false;
+		boolean exit = false;
+		String table = t[1];
+		
+		do{
+			if("computer".equals(table)) {
+				// Ici il faudra check la forme et les valeurs de chaque ajout
+				// Valeur null, erreur de syntaxe, nombre de champ
+				
+				ArrayList<String> fields = new ArrayList<String>();
+				System.out.print("Entrez une valeur pour le champ à mettre à jour. Laisser le champ vide pour ne rien modifier: ");
+				System.out.print("Entrez le nom: ");
+				fields.add(sc.nextLine());
+				System.out.print("Entrez la date d'achat: ");
+				fields.add(sc.nextLine());
+				System.out.print("Entrez la date de fin de production: ");
+				fields.add(sc.nextLine());
+	
+				// ici une formulation différente 
+				System.out.print("Entrez l'id de l'entreprise propritaire: ");			
+				fields.add(sc.nextLine());
+				err = false;
+				try {
+					wscmp.updateComputer(fields, t[2]);
+				}catch(IncorrectFieldException e) {
+					System.out.println("Erreur: Champ invalide");
+					err = true;
+				}catch (InstanceNotFoundException e) {
+					System.err.println("Erreur: Instance inexistante");
+					err = true;
+				}
+			} else if("company".equals(table)) {
+				String s = sc.nextLine();
+				String[] ss = s.replaceAll("[( | )]", " ").split(",");			
+				wscpy.create(Integer.valueOf(ss[0]), ss[1]);
+			}
+		}while(err & !exit);
 	}
 	
 	public void enterCommand() {
@@ -95,28 +154,39 @@ public class CLI_UI {
 			exit = true;
 		} else if("help".equals(command)) {
 			// On peut faire plus propre comme choix d'affichage d'options ( ArgParse )
-			System.out.println("Commands:");
-			System.out.println("show table_name id:     show details of instance in table_name where ID=id");
-			System.out.println("list [all] table_name:  list (all) instances in table_name");
-			System.out.println("create into table_name:  create a instance in table_name."
-					+ "\nThis require you to provide a tuple containing the values of what defines your object.");
+			System.out.println("Commandes:");
+			System.out.println("show table_name id:          show details of instance in table_name where ID=id");
+			System.out.println("list [all] table_name:       list (all) instances in table_name");
+			System.out.println("create into table_name:      create a instance in table_name."
+					+ "\n\t\t\t     This require you to provide a value for each attribute of the object.");
+			
+			System.out.println("delete [table name] id:      delete the instance in table_name where ID=id");
+			System.out.println("update [table name] id:      update the instance in table_name where ID=id"
+					+ "\n\t\t\t     This require you to provide a value for each attribute of the object.");
 			
 		} else if (command.contains("list")) {
-			// Ici target = objet visé par la requete i.e une des tables 
-			// i.e la suite de la chaine command, après le mot list	
 			target = command.split(" ");
 			list(target);
 			
 		} else if (command.contains("show")) {
 			target = command.split(" ");
 			System.out.println("Command show");
-			int id = Integer.valueOf(target[2]);
-			showDetailComputer(id);
+			showDetailComputer(target[2]);
 			
 		} else if (command.contains("create")) {
 			target = command.split(" ");
 			System.out.println("Command create");
 			create(target);
+			
+		} else if (command.contains("update")) {
+			target = command.split(" ");
+			System.out.println("Command delete");
+			update(target);
+			
+		} else if (command.contains("delete")) {
+			target = command.split(" ");
+			System.out.println("Command delete");
+			delete(target[2]);
 		}
 	}
 	

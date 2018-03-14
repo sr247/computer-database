8c242b951f18d54ea2f3479230b9faa6d439b5c4
Stéphane Romany <sromany@excilys.com>
2018-03-14 15:58:33 +0100

    Has modified Mapper Class to match Singleton Patter design

--- src/com/excilys/formation/java/mapper/CompanyMapper.java
+++ src/com/excilys/formation/java/mapper/CompanyMapper.java
@@ -3,77 +3,35 @@ package com.excilys.formation.java.mapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class CompanyMP {
+import com.excilys.formation.java.model.Company;
 
-	private int id;
-	private String name;
+public class CompanyMapper {
+
+	private static CompanyMapper _interface = null;
 	
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override 
-	public String toString() {
-		return "Company:("
-				+ "id=" + id + ", "
-				+ "name=" + name + ")";
+	private CompanyMapper() {
 		
 	}
 	
-	
-	
-	public CompanyMP(int id, String name) {
-		this.id = id;
-		this.name = name;
+	public static CompanyMapper getInterface() {
+		if(_interface == null) {
+			_interface = new CompanyMapper();
+		}
+		return _interface;
 	}
-
 	
-	public static CompanyMP map(ResultSet res) throws SQLException{
-		
-		int id = -1;
-		String name = "";
-
-		/*
+	public static Company map(ResultSet res) {
+		Company cpn = null;		
 		try {
+			int id = -1;
+			String name = "";
 			id = res.getInt("ID");
 			name = res.getString("NAME");
-		}catch(Exception e) {
+			cpn = new Company(id, name);
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		finally {
-			
-		}*/
-		
-		id = res.getInt("ID");
-		name = res.getString("NAME");
-		return new CompanyMP(id, name);
+		return cpn;
 	}
 
 }
--- src/com/excilys/formation/java/mapper/ComputerMapper.java
+++ src/com/excilys/formation/java/mapper/ComputerMapper.java
@@ -3,130 +3,16 @@ package com.excilys.formation.java.mapper;
 import java.sql.Date;
 import java.sql.ResultSet;
 
-public class ComputerMP {
-	
-	private int id;
-	private String name;
-	private Date introduced;
-	private Date discontinued;
+import com.excilys.formation.java.model.Computer;
 
-	// Ce champ deviendra un objet company à part entière.
-	private int companyId;
+public class ComputerMapper {
 	/*
 	 * Fonctionnalité  1 à tester immédiatement 
 	 * vérifier le lien avec JDBC (persitance)
 	 */
-	public ComputerMP(int id, String name, Date introduced, Date discontinued, int company_id) {
-		this.id = id;
-		this.name = name;
-		this.introduced = introduced;
-		this.discontinued = discontinued;
-		this.companyId = company_id;
-	}
 
 	
-	/**
-	 * @return the id
-	 */
-	public int getId() {
-		return id;
-	}
-
-
-
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	/**
-	 * @return the introduced
-	 */
-	public Date getIntroduced() {
-		return introduced;
-	}
-
-
-
-	/**
-	 * @param introduced the introduced to set
-	 */
-	public void setIntroduced(Date introduced) {
-		this.introduced = introduced;
-	}
-
-
-
-	/**
-	 * @return the discontinued
-	 */
-	public Date getDiscontinued() {
-		return discontinued;
-	}
-
-
-
-	/**
-	 * @param discontinued the discontinued to set
-	 */
-	public void setDiscontinued(Date discontinued) {
-		this.discontinued = discontinued;
-	}
-
-
-
-	/**
-	 * @return the company_id
-	 */
-	public int getCompanyId() {
-		return companyId;
-	}
-
-
-
-	/**
-	 * @param company_id the company_id to set
-	 */
-	public void setCompany_id(int company_id) {
-		this.companyId = company_id;
-	}
-	
-	@Override 
-	public String toString() {
-		return "Computer:("
-				+ "id=" + id + ", "
-				+ "name=" + name + ", "
-				+ "introduced=" + introduced + ", "
-				+ "discontinued=" + discontinued + ", "
-				+ "company_id" + companyId + ")";
-		
-	}
-
-	
-	
-	public static ComputerMP map(ResultSet res) {
+	public static Computer map(ResultSet res) {
 		
 		// Il faudrait check ici : Il y a surement un moyen plus �l�gant de default/catch
 		int id = -1;
@@ -136,11 +22,13 @@ public class ComputerMP {
 		int company_id  = -1; 
 		
 		try {
-			id = res.getInt("ID");
-			name = res.getString("NAME");
-			introduced = res.getDate("INTRODUCED");
-			discontinued = res.getDate("DISCONTINUED");
-			company_id = res.getInt("COMPANY_ID");
+			if(res.next()) {
+				id = res.getInt("ID");
+				name = res.getString("NAME");
+				introduced = res.getDate("INTRODUCED");
+				discontinued = res.getDate("DISCONTINUED");
+				company_id = res.getInt("COMPANY_ID");
+			}
 			
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -149,7 +37,7 @@ public class ComputerMP {
 			
 		}
 		
-		return new ComputerMP(id, name, introduced, discontinued, company_id);
+		return new Computer(id, name, introduced, discontinued, company_id);
 	}
 	
 }
